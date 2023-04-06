3f434c8230e572cdec32e5417a89b92e2ae6d682
Obadara16 <obadararoqeeb@gmail.com>
2023-04-06 11:56:28 +0100

    feat: changed to production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import replace from '@rollup/plugin-replace';
-import dotenv from 'dotenv';
+import replace from "@rollup/plugin-replace";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": "http://localhost:5000/",
+      "/api/v1": "https://afrimart-backend.onrender.com/",
     },
   },
   plugins: [
     react(),
     replace({
-      'process.env.STRIPE_SECRET_KEY': JSON.stringify(process.env.STRIPE_SECRET_KEY),
-      'process.env.MY_VARIABLE': JSON.stringify(process.env.MY_VARIABLE),
+      "process.env.STRIPE_SECRET_KEY": JSON.stringify(
+        process.env.STRIPE_SECRET_KEY
+      ),
+      "process.env.MY_VARIABLE": JSON.stringify(process.env.MY_VARIABLE),
       preventAssignment: true,
-    })
+    }),
   ],
   esbuild: {
     // this will allow dynamic require statements in faker to work correctly
